93d7cbbec2e559519c04b28295cdb8f7a63c885f
kickin_bear <kickin_bear@78d1cd67-304c-0410-b47f-9387fe7a7264>
2008-06-22 19:43:39 +0000

    switched to 2-clause BSD license

--- examples/toimage/main.cpp
+++ examples/toimage/main.cpp
@@ -24,73 +24,54 @@ int main(int argc, char *argv[])
 		cerr << "Can't open file for reading: " << filename << endl;
 		return 1;
 	}
-	// read data
-	Reader reader(f);
-	if (reader.getModule() == NULL)
-	{
-		cerr << "File in wrong format or corrupted: " << filename << endl;
-		return 1;
-	}
-
 	int w, h;
 	w = h = 512;
 	// allocate memory for height data
 	Real *data = new Real[w*h];
-	// create optimal pipeline (using one thread per CPU avaible)
-	Pipeline2D *pipeline2D = System::createOptimalPipeline2D ();
-	// add module to pipeline
-	noisepp::ElementID id = reader.getModule()->addToPipeline (pipeline2D);
-	// get a pointer to the main element
-	noisepp::PipelineElement2D *element = pipeline2D->getElement(id);
-
-	Real xmin = 0;
-	Real ymin = 0;
-	Real width = 5;
-	Real height = 5;
-
-	// calculate delta values
-	Real xDelta = width / Real(w);
-	Real yDelta = height / Real(h);
-
-	Real y = ymin;
-	// add the jobs to queue
-	for (int yi=0;yi<h;++yi)
-	{
-		pipeline2D->addJob (new noisepp::LineJob2D (pipeline2D, element, xmin, y, w, xDelta, data+yi*w));
-		y += yDelta;
-	}
-	// execute the jobs
-	pipeline2D->executeJobs ();
-	// create a image
-	noisepp::utils::Image img;
-	img.create (w, h);
-
-	// colorize
-	noisepp::utils::GradientRenderer gradients;
-	gradients.addGradient (-1.0000, noisepp::utils::ColourValue (  0,   0, 128)/255.f); // deeps
-	gradients.addGradient (-0.2500, noisepp::utils::ColourValue (  0,   0, 255)/255.f); // shallow
-	gradients.addGradient ( 0.0000, noisepp::utils::ColourValue (  0, 128, 255)/255.f); // shore
-	gradients.addGradient ( 0.0625, noisepp::utils::ColourValue (240, 240,  64)/255.f); // sand
-	gradients.addGradient ( 0.1250, noisepp::utils::ColourValue ( 32, 160,   0)/255.f); // grass
-	gradients.addGradient ( 0.3750, noisepp::utils::ColourValue (224, 224,   0)/255.f); // dirt
-	gradients.addGradient ( 0.7500, noisepp::utils::ColourValue (128, 128, 128)/255.f); // rock
-	gradients.addGradient ( 1.0000, noisepp::utils::ColourValue (255, 255, 255)/255.f); // snow
-	gradients.renderImage (img, data);
-
-	delete pipeline2D;
-	pipeline2D = 0;
-	delete[] data;
-	data = 0;
-	f.close ();
-
 	try
 	{
+		// read data
+		Reader reader(f);
+		if (reader.getModule() == NULL)
+		{
+			cerr << "File in wrong format or corrupted: " << filename << endl;
+			delete[] data;
+			return 1;
+		}
+	
+		noisepp::utils::PlaneBuilder2D builder;
+		builder.setModule (reader.getModule());
+		builder.setSize (w, h);
+		builder.setBounds (0, 0, 5, 5);
+		builder.setDestination (data);
+		builder.build ();
+		
+		// create a image
+		noisepp::utils::Image img;
+		img.create (w, h);
+	
+		// colorize
+		noisepp::utils::GradientRenderer gradients;
+		gradients.addGradient (-1.0000, noisepp::utils::ColourValue (  0,   0, 128)/255.f); // deeps
+		gradients.addGradient (-0.2500, noisepp::utils::ColourValue (  0,   0, 255)/255.f); // shallow
+		gradients.addGradient ( 0.0000, noisepp::utils::ColourValue (  0, 128, 255)/255.f); // shore
+		gradients.addGradient ( 0.0625, noisepp::utils::ColourValue (240, 240,  64)/255.f); // sand
+		gradients.addGradient ( 0.1250, noisepp::utils::ColourValue ( 32, 160,   0)/255.f); // grass
+		gradients.addGradient ( 0.3750, noisepp::utils::ColourValue (224, 224,   0)/255.f); // dirt
+		gradients.addGradient ( 0.7500, noisepp::utils::ColourValue (128, 128, 128)/255.f); // rock
+		gradients.addGradient ( 1.0000, noisepp::utils::ColourValue (255, 255, 255)/255.f); // snow
+		gradients.renderImage (img, data);
+	
+		f.close ();
+
 		img.saveBMP (outfile.c_str());
 	}
 	catch (std::exception &e)
 	{
-		cerr << "Error while writing to " << outfile << ": " << e.what() << endl;
+		cerr << "Exception thrown: " << e.what() << endl;
 	}
-
-    return 0;
+	delete[] data;
+	data = 0;
+	
+	return 0;
 }
--- noisepp/utils/NoiseBuilders.h
+++ noisepp/utils/NoiseBuilders.h
@@ -10,9 +10,6 @@
 //    * Redistributions in binary form must reproduce the above copyright notice,
 //      this list of conditions and the following disclaimer in the documentation
 //      and/or other materials provided with the distribution.
-//    * Neither the name of the Noise++ Library's copyright owner nor the names
-//      of its contributors may be used to endorse or promote products derived
-//      from this software without specific prior written permission.
 //
 // THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
 // AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
@@ -100,6 +97,11 @@ class Builder
 		void setDestination (Real *dest);
 		/// Sets the source module.
 		void setModule (Module *module);
+		/// Sets the source module.
+		inline void setModule (Module &module)
+		{
+			setModule (&module);
+		}
 		/// Sets a callback
 		void setCallback (BuilderCallback *callback);
 		/// Build.
